1efe646542da05c6975bfd207bb7ddcecf42964c
All <allmacdev@gmail.com>
2024-10-08 16:36:14 -0300

    aula:Importações Flexíveis

--- src/app/index/styles.ts
+++ src/app/index/styles.ts
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import { colors } from "../../styles/colors";
+import { colors } from "@/styles/colors";
 
 export const style = StyleSheet.create({
   container: {
